ed898565f4462d1ec0627fee86c623a074ffc47f
Chaitanya <chaitanyagupta172004@gmail.com>
2025-05-23 22:35:11 +0000

    feat: add navbar and animation

---
+++ components/container.tsx
@@ -0,0 +1,18 @@
+import {cn} from "@/lib/utils";
+import React from "react";
+
+const Container = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("mx-auto w-full max-w-4xl bg-white", className)}>
+      {children}
+    </div>
+  );
+};
+
+export default Container;
---
+++ components/navbar/index.tsx
@@ -0,0 +1,83 @@
+"use client";
+import React, {useState} from "react";
+import Container from "../container";
+import Image from "next/image";
+import Link from "next/link";
+import {motion, useMotionValueEvent, useScroll} from "motion/react";
+
+const Navbar = () => {
+  const navItems = [
+    {
+      title: "Whoami",
+      href: "/whoami",
+    },
+    {
+      title: "Projects",
+      href: "/projects",
+    },
+    {
+      title: "Contact",
+      href: "/contact",
+    },
+    {
+      title: "Blog",
+      href: "/blog",
+    },
+  ];
+
+  const [hovered, setHovered] = useState<number | null>(null);
+  const {scrollYProgress} = useScroll();
+
+  const [scrolled, setScrolled] = useState<boolean>(false);
+
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    setScrolled(latest > 0.2);
+  });
+
+  return (
+    <Container>
+      <motion.nav
+        animate={{
+          boxShadow: scrolled ? "var(--shadow-aceternity)" : "none",
+          width: scrolled ? "50%" : "100%",
+          y: scrolled ? 10 : 0,
+        }}
+        transition={{
+          duration: 0.3,
+          ease: "linear",
+        }}
+        className="fixed inset-x-0 top-0 z-50 mx-auto flex w-full max-w-4xl items-center justify-between rounded-full bg-white px-3 py-2 dark:bg-neutral-900"
+      >
+        <Image
+          src="/profile.jpg"
+          alt="avatar"
+          width={100}
+          height={100}
+          className="h-10 w-10 rounded-full"
+        />
+
+        <div className="flex items-center">
+          {navItems.map((item, idx) => (
+            <Link
+              className="relative w-full px-2 py-1 text-sm"
+              key={idx}
+              href={item.href}
+              onMouseEnter={() => setHovered(idx)}
+              onMouseLeave={() => setHovered(null)}
+            >
+              {hovered === idx && (
+                <motion.span
+                  className="absolute inset-0 h-full w-full rounded-md bg-neutral-100 dark:bg-neutral-800"
+                  layoutId="hovered-span"
+                />
+              )}
+              <span className="relative z-10">{item.title}</span>
+            </Link>
+          ))}
+        </div>
+      </motion.nav>
+    </Container>
+  );
+};
+
+export default Navbar;
