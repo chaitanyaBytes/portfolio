da0b25550f77ba8c566d0b5fea0a13aaffda4370
Chaitanya <chaitanyagupta172004@gmail.com>
2025-05-24 18:02:43 +0000

    feat: add projects page

---
+++ app/projects/page.tsx
@@ -0,0 +1,22 @@
+import Container from "@/components/container";
+import Projects from "@/components/projects";
+
+export default function Whoami() {
+  return (
+    <div className="flex min-h-screen flex-col items-start justify-start">
+      <Container className="min-h-[200vh] px-10 md:pt-20 md:pb-10">
+        <h1 className="text-primary text-2xl font-bold tracking-tight md:text-4xl">
+          Projects
+        </h1>
+        <p className="text-secondary max-w-lg pt-4 text-sm md:text-sm">
+          I am a software engineer who loves building scalable and efficient
+          systems. I am currently working at{" "}
+          <a href="https://www.google.com" className="text-primary">
+            Google
+          </a>
+        </p>
+        <Projects />
+      </Container>
+    </div>
+  );
+}
